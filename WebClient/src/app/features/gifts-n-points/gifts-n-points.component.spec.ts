bc29b4c0defd827f1ab0b89a28405940c6c1b829
Yurii Shchehliuk <shchehliuk124@gmail.com>
2022-06-02 15:01:53 +0200

    angular update to 11

--- WebClient/src/app/features/gifts-n-points/gifts-n-points.component.spec.ts
+++ WebClient/src/app/features/gifts-n-points/gifts-n-points.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
 
 import { GiftsNPointsComponent } from './gifts-n-points.component';
 
@@ -6,7 +6,7 @@ describe('GiftsNPointsComponent', () => {
   let component: GiftsNPointsComponent;
   let fixture: ComponentFixture<GiftsNPointsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GiftsNPointsComponent ]
     })
